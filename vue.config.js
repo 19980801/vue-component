0cbc2ac43655259b7cd446b201f872e1d5376ed7
yangyan123 <1252430435@qq.com>
2021-02-04 17:30:15 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-module.exports={
-  chainWebpack:config=>{
-    // 如果你使用的是sass，则此处应该为
-    // const oneOfsMap = config.module.rule('sass').oneOfs.store
-    // 依此类推，其他的比如stylus等一样用法
-    const oneOfsMap = config.module.rule('less').oneOfs.store;
-    oneOfsMap.forEach(item=>{
-      item.use()
-    });
-  }
-}
+// module.exports={
+//   chainWebpack:config=>{
+//     // 如果你使用的是sass，则此处应该为
+//     // const oneOfsMap = config.module.rule('sass').oneOfs.store
+//     // 依此类推，其他的比如stylus等一样用法
+//     const oneOfsMap = config.module.rule('less').oneOfs.store;
+//     oneOfsMap.forEach(item=>{
+//       item.use()
+//     });
+//   }
+// }
